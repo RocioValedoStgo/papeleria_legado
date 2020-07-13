54a8a5000646225d77e7f4cdf702c40356a26ce2
Rocío Valedo Santiago <181098@ids.upchiapas.edu.mx>
2020-07-13 14:29:40 -0500

    Added views

--- src/papeleria_legado/Main.java
+++ src/papeleria_legado/Main.java
@@ -1,9 +1,9 @@
 package papeleria_legado;
 
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
@@ -15,16 +15,14 @@ public class Main extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Label label =  new Label("Connection to MySQL: "+MySQLConnection.getConnection());
-		Group root = new Group();
-		root.getChildren().addAll(label);
-		label.setLayoutX(120);
-		label.setLayoutY(200);
-        Scene scene = new Scene(root, 640, 400);
-        primaryStage.setScene(scene);
-        primaryStage.getIcons().add(new Image("/assets/images/legado_papeleria.jpeg"));
+		Parent root = FXMLLoader.load(getClass().getResource("../views/Login.fxml"));
+		primaryStage.getIcons().add(new Image("/assets/images/legado_papeleria.jpeg"));
         primaryStage.setTitle("Papeleria Legado");
+        Scene scene = new Scene(root, 1000, 600);
+        primaryStage.setScene(scene);
         primaryStage.show();
+        
+        System.out.println("Connection to MySQL: "+MySQLConnection.getConnection());
 	}
 
 }
---
+++ src/papeleria_legado/RegisterController.java
@@ -0,0 +1,41 @@
+package papeleria_legado;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class RegisterController {
+
+	@FXML
+	private PasswordField txtPassword;
+
+	@FXML
+	private TextField txtName;
+
+	@FXML
+	private TextField txtLastName;
+
+	@FXML
+	private TextField txtTelephone;
+
+	@FXML
+	private TextField txtUsername;
+
+	@FXML
+	private TextField txtEmail;
+
+	@FXML
+	private ComboBox<?> cbTurn;
+
+	@FXML
+	void clickedBack(MouseEvent event) {
+
+	}
+
+	@FXML
+	void clickedRegister(MouseEvent event) {
+
+	}
+}
